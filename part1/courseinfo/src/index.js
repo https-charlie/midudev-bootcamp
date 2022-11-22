090e18891418c6ce90d70dd981982e2fb5f999d7
charlie <11.carlosgmc@gmail.com>
2022-11-21 22:11:11 -0300

    tarea 1 finalizada

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header  = ({course}) =>{
+  return <h1>{course}</h1>
+}
+
+const Content  = ({part,exercises}) =>{
+  return <p>{part} {exercises}</p>
+}
+
+const Total   = ({exercisestotal}) =>{
+  return <p>Number of exercises {exercisestotal}</p>
+}
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,20 +22,15 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+ /* const part = ['Fundamentals of React','Using props to pass data','State of a component']*/
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+     <Header course={course} />
+     <Content part= {part1} exercises= {exercises1}/>
+     <Content part= {part2} exercises= {exercises2}/>
+     <Content part= {part3} exercises= {exercises3}/>
+     <Total exercisestotal= {exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
