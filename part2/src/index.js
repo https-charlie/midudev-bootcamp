e0f8e024a0af12b1f2794551df452d10791775fb
charlie <11.carlosgmc@gmail.com>
2022-11-24 21:48:13 -0300

    Redenderizado

--- part2/src/index.js
+++ part2/src/index.js
@@ -1,7 +1,39 @@
 import ReactDOM from "react-dom";
-
-import App from "./App"
+import { useState } from "react"; /* Es un hook */
 
 const rootElement = document.getElementById("root");
 
+const App = (props) => {
+
+  const [contadorValue, updateContador] = useState(14);
+
+/* Devuelve un array de dos posiciones */  
+/*const contador =  useState(5) 
+const contadorValue = contador[0]
+const updateContador = contador[1]*/
+
+/*setInterval(() =>{
+  updateContador(contadorValue + 1)
+},3000
+)*/
+
+
+  return (
+  <div>
+    <h1>{contadorValue}</h1>
+    <button onClick={() => {/*updateContador(contadorValue + 1)*/ 
+    updateContador(prevContador => {return prevContador+1}) //El updateContador no solo puede recibir el valor , sino una funciòn que traiga al ejecutar pase el nuevo valor que debe tener el state
+    
+    }}>Incrementar</button>
+  </div>
+  ) 
+}
+
+
+// updateContador(parametro1 el valor contadorValue, parametro2 ejecución de una función nueva)
+
+
 ReactDOM.render(<App />, rootElement);
+
+
+
